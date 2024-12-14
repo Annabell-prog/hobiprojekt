1905c651ed52eaf4c8945dda3d62c39f07faead1
OveKre <ove.kukemelk@vikk.ee>
2024-12-14 23:23:38 +0200

    As a customer, I want illustrated About Me page provides a clear and engaging introduction, offering visual context to the story behind the brand Closes #12

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Define routes for your HTML files
 app.get('/', (req, res) => {
